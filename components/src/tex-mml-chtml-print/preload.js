725de74f55e922945e36d0ae968a9809ae2b2923
dar <david.rivron@kelis.fr>
2021-01-18 16:14:21 +0100

    Specific configuration for printed output (Chrome printToPdf renderer)

---
+++ components/src/tex-mml-chtml-print/preload.js
@@ -0,0 +1,8 @@
+import {Loader} from '../../../js/components/loader.js';
+
+Loader.preLoad(
+  'loader', 'startup',
+  'core',
+  'input/tex', 'input/mml',
+  'output/chtml', 'output/chtml/fonts/tex.js'
+);
